30890051007a030b50743f372c7d773920b9ef35
mrdaniray <mrdaniraymay@gmail.com>
2024-04-01 11:22:14 -0500

    some

--- src/services/getUser.ts
+++ src/services/getUser.ts
@@ -1,4 +1,4 @@
-import type { UserProps } from "@/types/UserProps";
+import type { UserProps } from "../types/UserProps";
 // import type { ImagesProps } from "@/types/ImagesProps";
 
 async function getUser(extension: string) {
